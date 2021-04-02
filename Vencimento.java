44473644237eebc9ec27747c596589a2434d0b2f
aslac2020 <andredesenvolvedorti@gmail.com>
2021-04-02 10:08:43 -0300

    alterações

--- Vencimento.java
+++ Vencimento.java
@@ -8,30 +8,6 @@ class Vencimento {
         this.dataVencimento = dataVencimento;
     }
 
-    // public String toString() {
-    //     int diaMes = dataVencimento.getDayOfMonth();
-    //     int mes = dataVencimento.getMonthValue();
-    //     int ano = dataVencimento.getYear();
-    //     return "Ingrediente: " + nome + " válido até " + diaMes + "/" + mes + "/" + ano;
-    // }
-
-    // public String getNome() {
-    //     return nome;
-    // }
-
-    // public void setNome(String nome) {
-    //     this.nome = nome;
-    // }
-
-    // public LocalDate getDataVencimento() {
-    //     return dataVencimento;
-    // }
-
-    // public void setDataValidade(LocalDate dataVencimento) {
-    //     this.dataVencimento = dataVencimento;
-    // }
-
-
     protected String nome;
     protected LocalDate dataVencimento;
 }
